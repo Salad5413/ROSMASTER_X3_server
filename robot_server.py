42703e1c07e5f07f4e11cc27a539ccb6c4cd3043
Salad5413 <56410863+Salad5413@users.noreply.github.com>
2023-04-13 20:42:01 -0400

    Update robot_server.py

--- robot_server.py
+++ robot_server.py
@@ -1,22 +1,35 @@
-import os
-import signal
-import subprocess
-import time
+from Rosmaster_Lib import Rosmaster
+# Create the Rosmaster object bot
+bot = Rosmaster()
 
+import asyncio
+import websockets
+import nest_asyncios
 
+nest_asyncio.apply()  # Apply nested asyncio patch
 
-command_to_execute = "roslaunch yahboomcar_laser laser_Warning.launch"
+def calculate_motion(degree):
+    if -5 < degree < 5:
+        return 0.0
+    else:
+        return max(min(degree / 40.0 * 0.3, 0.3), -0.3)
 
-# Start the process with a new session
-process = subprocess.Popen(command_to_execute, shell=True, preexec_fn=os.setsid)
+async def process_messages(websocket, path):
+    # Send a confirmation message to the iOS app
+    await websocket.send("Connected")
 
-# Run the process for a certain amount of time (e.g., 10 seconds)
-time.sleep(20)
+    async for message in websocket:
+        degree_lr, degree_fb = map(int, message.split(','))
+        #print(f"Degree LR: {degree_lr}, Degree FB: {degree_fb}")
 
-# Send the SIGINT signal (Ctrl+C) to the process group
-os.killpg(os.getpgid(process.pid), signal.SIGINT)
+        x = -calculate_motion(degree_fb)
+        y = calculate_motion(degree_lr)
+        z = 0.0
 
-# Wait for the process to finish and get the return code
-return_code = process.wait()
+        #print(x, y)
+        bot.set_car_motion(x, y, z)
 
-print(f"Process terminated with return code {return_code}")
+start_server = websockets.serve(process_messages, "0.0.0.0", 7003)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
